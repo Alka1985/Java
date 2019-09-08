c979092f34b09e452ec5f5a16c7268355af7d0ab
Alka1985 <54988008+Alka1985@users.noreply.github.com>
2019-09-08 11:55:19 +0200

    Create printsomething.java

---
+++ printsomething.java
@@ -0,0 +1,6 @@
+public class printsomething {
+  public static void main(String[] args) {
+    String aName = new String("Adam");
+    System.out.println("The String aName has the value: " + aName);
+  }
+}
